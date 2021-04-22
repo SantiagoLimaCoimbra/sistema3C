5591d2c10f8895c01f83e6a875d4432bf4e50af4
SantiagoLimaCoimbra <santiagolimacoimbra@gmail.com>
2021-04-22 08:53:04 -0300

    Subindo arquivos para o repositorio

---
+++ src/main/java/br/com/sistema/controller/FuncionarioController.java
@@ -0,0 +1,28 @@
+package br.com.sistema.controller;
+
+import br.com.sistema.model.Funcionario;
+import br.com.sistema.service.FuncionarioServiceImple;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FuncionarioController {
+
+    @Autowired
+    FuncionarioServiceImple funcionarioService;
+
+    @GetMapping("/funcionario/list")
+    public String list(Model model){
+        model.addAttribute("funcionarios", funcionarioService.findAll());
+        return "funcionario/list";
+    }
+
+    @GetMapping("/funcionario/add")
+    public String add(Model model){
+        model.addAttribute("funcionario", new Funcionario());
+        return "funcionario/add";
+    }
+
+}
